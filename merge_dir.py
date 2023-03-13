7b9c5b8e0d4d77fc1e1b8a24cb6e7b6dbba1dad3
Leslie Joevenller <95075819+shjoejoe@users.noreply.github.com>
2023-03-13 19:13:35 +0100

    Add files via upload

---
+++ merge_dir.py
@@ -0,0 +1,21 @@
+import imageio
+import os
+from PIL import Image
+from resizeimage import resizeimage
+import numpy as np 
+
+path = "/Users/shejoev/Documents/scan/CT-1/log_1-1/Denoised" # change this to your folder path
+image_path_list = os.listdir(path) # get a list of all files in the folder
+with imageio.get_writer("new_image.tiff") as new_image: # create a new .tiff image
+
+    for image_path in image_path_list: # loop through each file
+        if image_path.endswith(".tiff"): # check if it is a .tiff file
+            with open(path + "/" + image_path, "r+b") as f: # open the file as binary mode
+                with Image.open(f) as image: # read the file as an image using PIL
+                    resized_image = resizeimage.resize_cover(image, [200, 200]) # resize the image to 200x200 pixels using python-resize-image
+                    resized_array = np.asarray(resized_image) # convert the resized image to a numpy array using numpy.asarray()
+                    new_image.append_data(resized_array) # append the resized array data to the new image inside the with block
+
+
+
+
