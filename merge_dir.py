b2e6fd88bdfc92994203da10f842538f884675ba
Leslie Joevenller <95075819+shjoejoe@users.noreply.github.com>
2023-04-05 11:40:15 +0200

    Update merge_dir.py
    Add explanation to pixel sizes

--- merge_dir.py
+++ merge_dir.py
@@ -14,7 +14,7 @@ with imageio.get_writer("new_image.tiff") as new_image: # create a new .tiff ima
         if image_path.endswith(".tiff") or image_path.endswith(".tif"): # check if it is a .tiff file
             with open(path + "/" + image_path, "r+b") as f: # open the file as binary mode
                 with Image.open(f) as image: # read the file as an image using PIL
-                    resized_image = resizeimage.resize_cover(image, [200, 200]) # resize the image to 200x200 pixels using python-resize-image
+                    resized_image = resizeimage.resize_cover(image, [200, 200]) # resize the image to 200x200 pixels other values are applicable
                     resized_array = np.asarray(resized_image) # convert the resized image to a numpy array using numpy.asarray()
                     new_image.append_data(resized_array) # append the resized array data to the new image inside the with block
 
